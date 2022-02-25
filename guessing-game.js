5abe6d0173f593780883255c9cae497054bdac66
CavinTimothy <cavintimotht2014@gmail.com>
2022-02-24 20:26:23 -0800

    handing off to muhammad

---
+++ guessing-game.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let secretNumber = 10;
+
+const checkGuess = num => {
+if(typeof num === "number") return true;
+return false;
+}
+
+const askGuess = () => {
+  rl.question("Guess a number: ", guess => {
+
+    if(!checkGuess(guess)) {
+      console.log("Enter an actual number");
+      askGuess();
+    }
+
+    // guess = Number(guess);
+
+    if(guess == secretNumber) {
+      console.log("Correct!");
+      rl.close();
+    }
+    else if(guess > secretNumber) {
+      console.log("Too high");
+      askGuess();
+    }
+    else if(guess < secretNumber) {
+      console.log("Too low");
+      askGuess();
+    }
+  });
+  console.log("YOU WIN");
+}
+
+askGuess();
