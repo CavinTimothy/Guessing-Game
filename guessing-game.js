17ee28845cf55524a4fab9b58923ee54f2f74360
Muhammad <chelseafc56057@gmail.com>
2022-02-25 10:07:36 +0500

    firstPart

--- guessing-game.js
+++ guessing-game.js
@@ -7,35 +7,42 @@ const rl = readline.createInterface({
 
 let secretNumber = 10;
 
-const checkGuess = num => {
-if(typeof num === "number") return true;
-return false;
-}
+// const checkGuess = num => {
+// if(typeof num === "number") return true;
+// return false;
+// }
 
 const askGuess = () => {
   rl.question("Guess a number: ", guess => {
 
-    if(checkGuess(guess) === false) {
+    if(!Number(guess)){
+
+      // console.log(typeof Number(guess));
       console.log("Enter an actual number");
       askGuess();
     }
+
 
     guess = Number(guess);
 
     if(guess === secretNumber) {
       console.log("Correct!");
       rl.close();
+      // console.log("Correct!");
+      // console.log("YOU WIN");
     }
     else if(guess > secretNumber) {
-      console.log("Too high");
+      console.log("Too high")
       askGuess();
+      // console.log("Too high")
     }
     else if(guess < secretNumber) {
       console.log("Too low");
       askGuess();
+      // console.log("Too low");
     }
   });
-  console.log("YOU WIN");
+  //
 }
 
 askGuess();
