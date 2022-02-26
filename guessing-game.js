ee676ca3e82536f86b0c937b38e8ef5d308ffd92
Muhammad <chelseafc56057@gmail.com>
2022-02-26 22:53:55 +0500

    second-part

--- guessing-game.js
+++ guessing-game.js
@@ -5,14 +5,38 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let secretNumber = 10;
+// let secretNumber = askRange();
 
 const checkGuess = num => {
 if(!Number(num)) return false;
 return true;
 }
-
+const randomInRange = (min,max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
+const askRange = () =>{
+  let mini;
+  let maxi;
+  rl.question("Enter a min", mini =>{
+    rl.question("Enter a max", maxi =>{
+      console.log(`I'm thinking of a number between ${mini} and ${maxi}`);
+      console.log(randomInRange(mini,maxi));
+      askGuess();
+      // rl.close();
+    })
+  })
+  // return randomInRange(mini,maxi);
+}
+// const randomInRange = (min,max) => {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random()*(max-min+1)+min)
+// }
+let secretNumber;
 const askGuess = () => {
+  secretNumber = askRange();
   rl.question("Guess a number: ", guess => {
 
     if(!checkGuess(guess)){
